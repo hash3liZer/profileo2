2154be2f41a6995f1df626e8834f12cf92cfc220
Shameer Kashif (Shiri) <me@shameerkashif.me>
2025-03-21 14:49:58 +0500

    Update Tabsbar.jsx

--- components/Tabsbar.jsx
+++ components/Tabsbar.jsx
@@ -4,12 +4,12 @@ import styles from '../styles/Tabsbar.module.css';
 const Tabsbar = () => {
   return (
     <div className={styles.tabs}>
-      <Tab icon="/react_icon.svg" filename="home.jsx" path="/" />
-      <Tab icon="/html_icon.svg" filename="about.html" path="https://blog.shameerkashif.me/resume" />
-      <Tab icon="/css_icon.svg" filename="contact.css" path="/contact" />
+      <Tab icon="/react_icon.svg" filename="_app.jsx" path="/" />
+      <Tab icon="/html_icon.svg" filename="something_about_me.html" path="https://blog.shameerkashif.me/resume" />
+      <Tab icon="/css_icon.svg" filename="write_me_an_email.css" path="/contact" />
       {/* <Tab icon="/js_icon.svg" filename="projects.js" path="/projects" /> */}
-      <Tab icon="/json_icon.svg" filename="articles.json" path="https://blog.shameerkashif.me" />
-      <Tab icon="/markdown_icon.svg" filename="github.md" path="/github" />
+      <Tab icon="/json_icon.svg" filename="__data.json" path="https://blog.shameerkashif.me" />
+      <Tab icon="/markdown_icon.svg" filename="cracked_github.md" path="/github" />
     </div>
   );
 };
